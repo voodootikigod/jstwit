6730dc3e398e509d815fc721545d1248877d624e
Chris Williams <chris@iterativedesigns.com>
2009-07-03 12:48:13 -0400

    in ur request, inspecting ur objects.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,7 +1,13 @@
-function main( aRequest ) {
-  var str = "";
-  for (i in aRequest) {
-    str += (i+":"+aRequest[i]+"<br/>");
+function render_object(o, base_depth) {
+  str = "";
+  for (i in o) {
+    str += (base_depth+i+":"+o[i]+"<br/>");
+    if (typeof o[i] == "object") {
+      render_object(o[i], base_depth+"&nbsp;&nbsp;");
+    }
   }
-  return str;
+}
+
+function main( aRequest ) {
+  return render_object(aRequest, "");
 }
